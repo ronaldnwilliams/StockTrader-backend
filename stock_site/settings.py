9efb73987ab32ac07b14dadfcf4ceced1e19aee6
Ronald Williams <ronaldnwilliams@gmail.com>
2018-06-17 17:03:22 -0500

    added postgres settings

--- stock_site/settings.py
+++ stock_site/settings.py
@@ -90,13 +90,12 @@ WSGI_APPLICATION = 'stock_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -166,4 +165,7 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
 
+# heroku settings
 django_heroku.settings(locals())
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
