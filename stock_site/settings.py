37137f6fb67210a40f48c59a6d934ddab5f454fd
Ronald Williams <ronaldnwilliams@gmail.com>
2018-06-21 15:19:13 -0500

    updated settings.py

--- stock_site/settings.py
+++ stock_site/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://stock-site-rnw.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'stock-site-rnw.herokuapp.com']
 
 
 # Application definition
