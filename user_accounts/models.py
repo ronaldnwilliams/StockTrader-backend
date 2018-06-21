fe1732624f7914d5f71f13e0690e7da4a0adf6ba
Ronald Williams <ronaldnwilliams@gmail.com>
2018-06-21 15:50:51 -0500

    updated requirements.txt

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -6,16 +6,9 @@ class User_Account(models.Model):
     account_user = models.OneToOneField(User, related_name='user_account', on_delete=models.CASCADE)
     joined = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return "{}:{}".format(self.account_user, self.joined)
-
 class Portfolio(models.Model):
     user_account = models.OneToOneField(User_Account, related_name='portfolio', on_delete=models.CASCADE)
     cash = models.DecimalField(max_digits=19, decimal_places=2, default=100000.00)
-
-    def __str__(self):
-        return "cash: {}, stocks: {}, daily balance: {}, stocks sold: {}".format(
-        self.cash, self.stocks.all(), self.daily_balance.all())
 
 class Daily_Balance(models.Model):
     portfolio = models.ForeignKey(Portfolio, related_name='daily_balance', on_delete=models.CASCADE)
