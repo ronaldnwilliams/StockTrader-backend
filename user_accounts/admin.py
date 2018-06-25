0a6210ecba936821bf8d8a4f6617a81b8fcb423f
Ronald Williams <ronaldnwilliams@gmail.com>
2018-06-25 11:54:10 -0500

    admin.py added inlines to portfolio for user_accounts.

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -3,8 +3,23 @@ from django.contrib import admin
 from .models import *
 
 
+class Daily_BalanceInline(admin.TabularInline):
+    model = Daily_Balance
+
+class Intraday_BalanceInline(admin.TabularInline):
+    model = Intraday_Balance
+
+class Watch_StockInline(admin.TabularInline):
+    model = Watch_Stock
+
+class User_StockInline(admin.TabularInline):
+    model= User_Stock
+
 class PortfolioInline(admin.TabularInline):
     model = Portfolio
+
+    inlines = [User_StockInline, Watch_StockInline, Intraday_BalanceInline,
+    Daily_BalanceInline,]
 
 class User_AccountAdmin(admin.ModelAdmin):
     inlines = [PortfolioInline,]
@@ -13,4 +28,5 @@ admin.site.register(User_Account, User_AccountAdmin)
 admin.site.register(User_Stock)
 admin.site.register(Watch_Stock)
 admin.site.register(Daily_Balance)
+admin.site.register(Intraday_Balance)
 admin.site.register(Portfolio)
