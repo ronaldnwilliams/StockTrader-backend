92ce4bfe7b6949e8ca7f9ad10193eafe8a588ca7
Ronald Williams <ronaldnwilliams@gmail.com>
2018-07-09 15:38:56 -0500

    comment out a line in settings.py.

--- stock_site/settings.py
+++ stock_site/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-heroku settings
+# heroku settings
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
